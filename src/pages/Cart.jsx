8be2e37f695231193192ec5d589a3c14eac9760d
be-a-guptaji <aryanbaadlas@gmail.com>
2024-10-08 01:07:16 +0530

    minor change

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -64,23 +64,82 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { CardItem } from "./CardItem";
+import { MdOutlineDelete } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { remove } from "../redux/Slices/CartSlice";
+import { toast } from "react-toastify";
 
 export const Cart = () => {
   const { cart } = useSelector((state) => state);
   const [totalAmount, settotalAmount] = useState(0);
+  const [item, setitem] = useState([]);
+  let orders = [];
+
+  const dispatch = useDispatch();
+  
 
   useEffect(() => {
-    settotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+    getItem();
   }, [cart]);
+  
+  const getItem = async () => {
+    const res = await fetch("http://localhost:8080/cart", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cart),
+    });
+    //  item = await res.json();
+    const data = await res.json();
+    console.log(data.orders);
+    orders=[...data.orders]
+    setitem(...data.orders);
+    console.log(item);
+    settotalAmount(orders.reduce((acc, curr) => acc + curr.price, 0));
+  };
+  
+   const removeFromCart = (id) => {
+     dispatch(remove(id));
+     toast.error("Item removed");
+   };
   
   return (
     <div className="flex justify-between gap-7">
       {cart.length > 0 ? (
         <div className="flex justify-center w-full gap-[200px]">
           <div className="flex flex-col justify-between w-1/4 gap-4 my-12">
-            {cart.map((item) => {
-              return <CardItem key={item.id} item={item} />;
-            })}
+            {orders.map((item, index) => (
+              <div className="flex" key={index}>
+                {" "}
+                {/* Use index directly as key */}
+                <div className="flex gap-10 py-4 border-b border-gray-300">
+                  <div>
+                    <img src={item.image} alt="img" className="w-[200px]" />
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <h1 className="text-xl font-bold">{item.title}</h1>
+                    <p>
+                      {/* Check if description is defined before splitting */}
+                      {item.description
+                        ? item.description.split(" ").slice(0, 10).join(" ") +
+                          "..."
+                        : "No description available."}
+                    </p>
+                    <div className="font-extrabold text-xl text-green-600">
+                      ${item.price}
+                    </div>
+                  </div>
+                  <div
+                    onClick={() => removeFromCart(item._id)} // Assuming removeFromCart is defined
+                    className="flex mt-[80px] text-2xl justify-center items-center cursor-pointer rounded-full"
+                  >
+                    <MdOutlineDelete />
+                  </div>
+                </div>
+                <hr />
+              </div>
+            ))}
           </div>
           <div className="flex flex-col h-[80vh] justify-around">
             <div className="flex  flex-col  mt-16 gap-2">
@@ -92,17 +151,16 @@ export const Cart = () => {
                 <span className="">Total Items:{cart.length}</span>
               </p>
               <p className="text-black font-bold text-2xl">
-                Total Amount : {" "}
+                Total Amount :{" "}
                 <span className="text-green-600">
-
-                  {Math.round(totalAmount * 100 * 0.0070) / 100}
+                  {Math.round(totalAmount * 100 * 0.007) / 100}
                 </span>
-                  <span className="text-green-600 font-normal"> SOL</span>
+                <span className="text-green-600 font-normal"> SOL</span>
               </p>
               <Link to={"/checkout"}>
-              <button className="border border-green-600 px-16 rounded-md p-2 text-white bg-green-600 font-bold text-xl mt-6">
-                Checkout Now{" "}
-              </button>
+                <button className="border border-green-600 px-16 rounded-md p-2 text-white bg-green-600 font-bold text-xl mt-6">
+                  Checkout Now{" "}
+                </button>
               </Link>
             </div>
           </div>
